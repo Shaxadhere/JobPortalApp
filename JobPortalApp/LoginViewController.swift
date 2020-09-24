841037d6ca03a36f2cc72fcf07fb2bf1dbe0e760
Shehzad <shaxad.here@gmail.com>
2020-09-25 04:34:56 +0500

    initial commit

---
+++ JobPortalApp/LoginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LoginViewController.swift
+//  JobPortalApp
+//
+//  Created by Shehzad on 25/09/2020.
+//  Copyright © 2020 Shehzad. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
